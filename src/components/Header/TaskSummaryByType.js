b662e63580f9dafb15d3d5b86e7e24b5c51b2ae0
Petry <pierre.etryhard.pro@gmail.com>
2022-07-26 12:49:04 +0200

    keep cleaning old code

--- src/components/Header/TaskSummaryByType.js
+++ src/components/Header/TaskSummaryByType.js
@@ -5,11 +5,6 @@ import TaskContext from "../store/task-context";
 const TaskSummaryByType = (props) => {
   const ctxTask = useContext(TaskContext);
 
-  let other = 0;
-  let personal = 0;
-  let work = 0;
-  let shopping = 0;
-
   let countType = ctxTask.taskList.filter(
     (task) => task.type === props.type
   ).length;
